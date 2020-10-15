03288234f59a118e83f688644059605df121f928
Puja <pujayadav@gmail.com>
2020-10-15 09:00:16 +0530

    [puja] add . Welcome

---
+++ AnalyzerTesting/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
+
+namespace AnalyzerTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenMood_ReturnSad()
+        {
+            string expected = "SAD";
+            string message = "I am Sad now";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+       
+            string mood = moodAnalyser.AnalyseMood();
+            Assert.AreEqual(expected, mood);
+
+        }
+    }
+}
+    
+
+
---
+++ MoodAnalyzer/MoodAnalyser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyser
+    {
+        private string message;
+
+
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            if (this.message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
+
